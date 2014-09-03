4bd6b44729a3766276f445b88aa3b3aef3ec204f
Leo Chen-Tsu_Lin <lin19890808@gmail.com>
2014-09-03 17:50:57 +0800

    fix angular require bug

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,18 @@
-var angular = require('angular');
+require('angular');
+require('angular-route');
 var Route = require('./config/Route');
-var MainController = require('../controllers/MainController');
-var FrontMvcController = require('../controllers/FrontMvcController');
-var SpaController = require('../controllers/SpaController');
-var FrameworkController = require('../controllers/FrameworkController');
-var ProsConsController = require('../controllers/ProsConsController');
-var DataBindingController = require('../controllers/DataBindingController');
-var ServiceController = require('../controllers/ServiceController');
-var DiController = require('../controllers/DiController');
+var MainController = require('./controllers/MainController');
+var FrontMvcController = require('./controllers/FrontMvcController');
+var SpaController = require('./controllers/SpaController');
+var FrameworkController = require('./controllers/FrameworkController');
+var ProsConsController = require('./controllers/ProsConsController');
+var DataBindingController = require('./controllers/DataBindingController');
+var ServiceController = require('./controllers/ServiceController');
+var DiController = require('./controllers/DiController');
 
-angualr.module('sharing', [ 'ngRoute' ])
+console.log(angular);
+
+angular.module('app.sharing', [ 'ngRoute' ])
     .config(Route)
     .controller('MainController', MainController)
     .controller('FrontMvcController', FrontMvcController)
